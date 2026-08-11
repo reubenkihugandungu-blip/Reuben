afd4df1d64dc9ceeb938bbaee4d4c17e4c0b4c27
Rebbman <reubenkihugandungu@gmail.com>
2026-08-11 12:12:40 +0300

    Update week 4

--- week4/day2/try and except.py
+++ week4/day2/try and except.py
@@ -3,14 +3,14 @@
 
 steps_data = ["9200", "7500", "ten thousand", "8800", "6900"]
 
-for item in steps_data:
-    try:
-        steps = int(item)
-        if steps >= 8000:
+for item in steps_data: # starts a loop that goes through each value in the list steps_data. each element is assigned to the variable item on each iteration.
+    try:# begins a try block to run code that might raise an error. If an exception occurs inside this block, python will skip to the matching except
+        steps = int(item) # tries to convert the current item from a string to an integer, if item is not a valid numeric string this raises ValueError
+        if steps >= 8000:# checks whether the converted integer steps is atleast 8000
             print(steps, "- Goal hit")
         else:
             print(steps, "- Below goal")
-    except ValueError:
-        print(f"'{item}'' is not a valid number. Skipping.")
+    except ValueError:# this block runs only if converting item to int failed witha value error
+        print(f"'{item}'' is not a valid number. Skipping.")# prints a message saying the current item is invalid and wil be skipped.
 
 
